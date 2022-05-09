71b8fd56433593361d437f3df90f92fe79838972
tymon42 <tymon42@outlook.com>
2022-05-09 23:10:04 +0800

    ✨ feat: add swagger

--- go.mod
+++ go.mod
@@ -4,14 +4,30 @@ go 1.16
 
 require (
 	github.com/aliyun/alibaba-cloud-sdk-go v1.61.1526
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fox-one/mixin-sdk-go v1.6.2
 	github.com/gin-gonic/gin v1.7.7
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/gofrs/uuid v4.2.0+incompatible
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/shopspring/decimal v1.3.1
 	github.com/sirupsen/logrus v1.8.1
+	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
+	github.com/swaggo/gin-swagger v1.4.3 // indirect
+	github.com/swaggo/swag v1.8.1 // indirect
+	github.com/urfave/cli v1.20.0 // indirect
+	github.com/urfave/cli/v2 v2.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+	golang.org/x/tools v0.1.10 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gorm.io/driver/sqlite v1.3.1
 	gorm.io/gorm v1.23.3
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "2SOMEone/docs"
 	"2SOMEone/core"
 	"2SOMEone/middlewares"
 	"2SOMEone/service"
@@ -10,254 +11,360 @@ import (
 	"net/http"
 	"strconv"
 
+	gs "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/files"
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	dbc         = util.OpenDB("./2-some-one.db")
+	userService = service.NewUserService(dbc)
+	noteService = service.NewNoteService(dbc)
+)
+
+// @title BubbleBox
+// @version 1.0
+// @description NULL
+// @termsOfService http://swagger.io/terms/
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host https://api.2some.one
+// @BasePath 
 func main() {
-	// baseapi := "https://imgapi.leaper.one"
-	dbc := util.OpenDB("./2-some-one.db")
+	// dbc := util.OpenDB("./2-some-one.db")
 	gin.SetMode(gin.DebugMode)
 	// gin.SetMode(gin.ReleaseMode)
-
-	userService := service.NewUserService(dbc)
-	noteService := service.NewNoteService(dbc)
 
 	r := gin.Default()
 	r.Use(middlewares.Cors())
 
-	r.GET("/phonecode", func(c *gin.Context) {
-		ctx := context.Background()
-		phone := c.Query("phone")
-		code, err := userService.SendPhoneCode(ctx, phone)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2001,
-				"msg":  err.Error(),
-			})
-			// c.String(http.StatusBadGateway, fmt.Sprintf("参数错误:%v", phone))
-			return
-		}
-		fmt.Printf("code: %v\n", code)
-		fmt.Printf("err: %v\n", err)
-		// accounter.CreateByEmail(ctx, email, code)
-		// c.String(http.StatusOK, fmt.Sprintf("已向 %s 发送验证码 ", phone))
-		c.JSON(http.StatusOK, gin.H{
-			"code": 2000,
-			"msg":  "验证码已发送",
-		})
-	})
-
-	r.POST("/signup", func(c *gin.Context) {
-		ctx := context.Background()
-		var logup_user core.SignUpUser
-		err := c.ShouldBind(&logup_user)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2001,
-				"msg":  "无效的参数",
-			})
-			return
-		}
-
-		user, err := userService.SignUpByPhone(ctx, &logup_user)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2001,
-				"msg":  err.Error(),
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"code":  2000,
-			"phone": user.Phone,
-			"msg":   "注册成功",
-		})
-	})
-
-	r.POST("/setinfo", middlewares.JWTAuthMiddleware(), func(c *gin.Context) {
-		ctx := context.Background()
-		user_id := c.MustGet("user_id").(string)
-		var setinfo core.UserForMe
-		err := c.ShouldBind(&setinfo)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2001,
-				"msg":  "无效的参数",
-			})
-			return
-		}
-		err = userService.SetInfo(ctx, user_id, &setinfo)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2002,
-				"msg":  err,
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"code": 2000,
-			"msg":  "成功",
-		})
-	})
-
-	r.POST("/auth", func(c *gin.Context) {
-		ctx := context.Background()
-		var login_user core.LoginUser
-		err := c.ShouldBind(&login_user)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2001,
-				"msg":  "无效的参数",
-			})
-			return
-		}
-
-		// 校验用户名和密码是否正确
-		token, err := userService.Auth(ctx, &login_user)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2002,
-				// "msg":  "鉴权失败",
-				"msg": err.Error(),
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"code": 2000,
-			"msg":  "success",
-			"data": gin.H{"token": token},
-		})
-	})
-
-	r.POST("/to/:rname", middlewares.JWTAuthMiddleware(), func(c *gin.Context) {
-		ctx := context.Background()
-		sender_id := c.MustGet("user_id")
-		rname := c.Param("rname")
-		var tnote core.Note
-		err := c.ShouldBind(&tnote)
-		if err != nil || rname == "" {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2001,
-				"msg":  err.Error(),
-			})
-			return
-		}
-
-		tnote.Sender = sender_id.(string)
-		err = noteService.Create(ctx, tnote.ForStore(), rname)
-		if err != nil {
-			c.JSON(http.StatusBadGateway, gin.H{
-				"code": 2002,
-				"msg":  "失败",
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"code": 2000,
-			"msg":  "成功",
-		})
-	})
-
-	r.GET("/me", middlewares.JWTAuthMiddleware(), func(c *gin.Context) {
-		ctx := context.Background()
-		user_id := c.MustGet("user_id").(string)
-		user, err := userService.GetMe(ctx, user_id)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2002,
-				"msg":  err.Error(),
-			})
-			return
-		}
-		c.JSON(http.StatusOK, user) // 返回信息
-	})
-
-	r.GET("/user/:user_name", func(c *gin.Context) {
-		ctx := context.Background()
-		user_name := c.Param("user_name")
-		user, err := userService.VisitUser(ctx, user_name)
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2002,
-				"msg":  err.Error(),
-			})
-			return
-		}
-		c.JSON(http.StatusOK, user) // 返回信息
-	})
-
-	r.GET("/note/:note_id", middlewares.JWTAuthMiddleware(), func(c *gin.Context) {
-		ctx := context.Background()
-		note_id := c.Param("note_id")
-		note, err := noteService.GetByID(ctx, note_id)
-		if err != nil {
-			c.JSON(http.StatusForbidden, gin.H{
-				"code": 2002,
-				"msg":  err.Error(),
-			})
-			return
-		}
-		note, err = note.ForRead()
-		if err != nil {
-			c.JSON(http.StatusForbidden, gin.H{
-				"code": 2002,
-				"msg":  err.Error(),
-			})
-			return
-		}
-		c.JSON(http.StatusOK, note)
-
-	})
-
-	r.GET("/notes/received/", middlewares.JWTAuthMiddleware(), func(c *gin.Context) {
-		ctx := context.Background()
-		user_id := c.MustGet("user_id")
-		offset, _ := strconv.Atoi(c.DefaultQuery("offset", "1"))
-		limit, _ := strconv.Atoi(c.DefaultQuery("limit", "20"))
-		notes, count, err := noteService.RecipientGet(ctx, offset, limit, user_id.(string))
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2002,
-				"msg":  err.Error(),
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"code":   2000,
-			"msg":    "成功",
-			"count":  count,
-			"notes":  notes,
-			"offset": offset,
-			"limit":  limit,
-		})
-		// c.String(http.StatusOK,"yes")
-	})
-
-	r.GET("/notes/sent", middlewares.JWTAuthMiddleware(), func(c *gin.Context) {
-		ctx := context.Background()
-		user_id := c.MustGet("user_id")
-		offset_str := c.DefaultQuery("offset", "1")
-		offset, _ := strconv.Atoi(offset_str)
-		limit_str := c.DefaultQuery("limit", "20")
-		limit, _ := strconv.Atoi(limit_str)
-		notes, count, err := noteService.SenderGet(ctx, offset, limit, user_id.(string))
-		if err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"code": 2002,
-				"msg":  err.Error(),
-			})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"code":   2000,
-			"msg":    "成功",
-			"count":  count,
-			"notes":  notes,
-			"offset": offset,
-			"limit":  limit,
-		})
-
-	})
-
+	initRoutes(r)
 	r.Run(":3002")
+}
+
+func initRoutes(r *gin.Engine) {
+	r.GET("/swagger/*any", gs.WrapHandler(swaggerFiles.Handler))
+	r.GET("/phonecode", phoneCode)
+	r.POST("/signup", signUp)
+	r.POST("/setinfo", middlewares.JWTAuthMiddleware(), setInfo)
+	r.POST("/auth", auth)
+	r.GET("/to/:rame", middlewares.JWTAuthMiddleware(), to)
+	r.GET("/me", middlewares.JWTAuthMiddleware(), me)
+	r.GET("/user/:user_id", user)
+	r.GET("/note/:note_id", middlewares.JWTAuthMiddleware(),note)
+	r.GET("/notes/received",middlewares.JWTAuthMiddleware(), notesReceived)
+	r.GET("/notes/sent",middlewares.JWTAuthMiddleware(), notesSent)
+}
+
+// @Summary 获取手机验证码
+// @Description 向用户手机发送验证码
+// @Tags user
+// @Produce json
+// @Param phone query string true "手机号"
+// @Success 200 {string} string "{"code": 2000, "msg": ""}"
+// @Router /phonecode [get]
+func phoneCode(c *gin.Context) {
+	ctx := context.Background()
+	phone := c.Query("phone")
+	code, err := userService.SendPhoneCode(ctx, phone)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2001,
+			"msg":  err.Error(),
+		})
+		// c.String(http.StatusBadGateway, fmt.Sprintf("参数错误:%v", phone))
+		return
+	}
+	fmt.Printf("code: %v\n", code)
+	fmt.Printf("err: %v\n", err)
+	// accounter.CreateByEmail(ctx, email, code)
+	// c.String(http.StatusOK, fmt.Sprintf("已向 %s 发送验证码 ", phone))
+	c.JSON(http.StatusOK, gin.H{
+		"code": 2000,
+		"msg":  "验证码已发送",
+	})
+}
+
+// @Summary 用户注册
+// @Description 用户注册
+// @Tags user
+// @Accept json
+// @Produce json
+// @Success 200 {string} string "{"code": 2000, "msg": "注册成功"}"
+// @Router /signup [post]
+func signUp(c *gin.Context) {
+	ctx := context.Background()
+	var logup_user core.SignUpUser
+	err := c.ShouldBind(&logup_user)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2001,
+			"msg":  "无效的参数",
+		})
+		return
+	}
+
+	user, err := userService.SignUpByPhone(ctx, &logup_user)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2001,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code":  2000,
+		"phone": user.Phone,
+		"msg":   "注册成功",
+	})
+}
+
+// @Summary 设置用户信息
+// @Description 设置用户信息
+// @Tags user
+// @Accept json
+// @Produce json
+// @Success 200 {string} string "{"code": 2000, "msg": "成功"}"
+// @Router /setinfo [post]
+func setInfo(c *gin.Context) {
+	ctx := context.Background()
+	user_id := c.MustGet("user_id").(string)
+	var setinfo core.UserForMe
+	err := c.ShouldBind(&setinfo)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2001,
+			"msg":  "无效的参数",
+		})
+		return
+	}
+	err = userService.SetInfo(ctx, user_id, &setinfo)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2002,
+			"msg":  err,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 2000,
+		"msg":  "成功",
+	})
+}
+
+// @Summary 用户授权
+// @Description 用户授权
+// @Tags user
+// @Accept json
+// @Produce json
+// @Success 200 {string} string "{"code": 2000, "msg": "success", "data": {"token": ""}}"
+// @Router /auth [post]
+func auth(c *gin.Context) {
+	ctx := context.Background()
+	var login_user core.LoginUser
+	err := c.ShouldBind(&login_user)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2001,
+			"msg":  "无效的参数",
+		})
+		return
+	}
+
+	// 校验用户名和密码是否正确
+	token, err := userService.Auth(ctx, &login_user)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2002,
+			// "msg":  "鉴权失败",
+			"msg": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 2000,
+		"msg":  "success",
+		"data": gin.H{"token": token},
+	})
+}
+
+// @Summary 创建 Note
+// @Description 创建 Note
+// @Tags note
+// @Accept json
+// @Produce json
+// @Success 200 {string} string "{"code": 2000, "msg": ""}"
+// @Router /note [post]
+func to(c *gin.Context) {
+	ctx := context.Background()
+	sender_id := c.MustGet("user_id")
+	rname := c.Param("rname")
+	var tnote core.Note
+	err := c.ShouldBind(&tnote)
+	if err != nil || rname == "" {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2001,
+			"msg":  err.Error(),
+		})
+		return
+	}
+
+	tnote.Sender = sender_id.(string)
+	err = noteService.Create(ctx, tnote.ForStore(), rname)
+	if err != nil {
+		c.JSON(http.StatusBadGateway, gin.H{
+			"code": 2002,
+			"msg":  "失败",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 2000,
+		"msg":  "成功",
+	})
+}
+
+// @Summary 获取本用户信息
+// @Description 获取本用户信息
+// @Tags user
+// @Accept json
+// @Produce json
+// @Success 200 {string} string "{"code": 2000, "msg": "success", "data": {"user": {}}}"
+// @Router /user [get]
+func me(c *gin.Context) {
+	ctx := context.Background()
+	user_id := c.MustGet("user_id").(string)
+	user, err := userService.GetMe(ctx, user_id)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2002,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 2000,
+		"msg":  "success",
+		"data": gin.H{"user": user},
+	}) // 返回信息
+}
+
+// @Summary 访问用户信息
+// @Description 访问用户信息
+// @Tags user
+// @Accept json
+// @Produce json
+// @Success 200 {string} string "{"code": 2000, "msg": "success", "data": {"user": {}}}"
+// @Router /user/{user_id} [get]
+func user(c *gin.Context) {
+	ctx := context.Background()
+	user_name := c.Param("user_name")
+	user, err := userService.VisitUser(ctx, user_name)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2002,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 2000,
+		"msg":  "success",
+		"data": gin.H{"user": user},
+	}) // 返回信息
+}
+
+// @Summary 获取用户的Note
+// @Description 获取用户的Note
+// @Tags note
+// @Accept json
+// @Produce json
+// @Success 200 {string} string "{"code": 2000, "msg": "success", "data": {"notes": []}}"
+// @Router /note/{note_id} [get]
+func note(c *gin.Context) {
+	ctx := context.Background()
+	note_id := c.Param("note_id")
+	note, err := noteService.GetByID(ctx, note_id)
+	if err != nil {
+		c.JSON(http.StatusForbidden, gin.H{
+			"code": 2002,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	note, err = note.ForRead()
+	if err != nil {
+		c.JSON(http.StatusForbidden, gin.H{
+			"code": 2002,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 2000,
+		"msg":  "success",
+		"data": gin.H{"note": note},
+	})
+}
+
+// @Summary 获取收到的Note
+// @Description 获取收到的Note
+// @Tags note
+// @Accept json
+// @Produce json
+// @Router /note/received [get]
+func notesReceived(c *gin.Context) {
+	ctx := context.Background()
+	user_id := c.MustGet("user_id")
+	offset, _ := strconv.Atoi(c.DefaultQuery("offset", "1"))
+	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "20"))
+	notes, count, err := noteService.RecipientGet(ctx, offset, limit, user_id.(string))
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2002,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code":   2000,
+		"msg":    "成功",
+		"count":  count,
+		"notes":  notes,
+		"offset": offset,
+		"limit":  limit,
+	})
+}
+
+// @Summary 获取发出的Note
+// @Description 获取发出的Note
+// @Tags note
+// @Accept json
+// @Produce json
+// @Router /note/sent [get]
+func notesSent(c *gin.Context) {
+	ctx := context.Background()
+	user_id := c.MustGet("user_id")
+	offset_str := c.DefaultQuery("offset", "1")
+	offset, _ := strconv.Atoi(offset_str)
+	limit_str := c.DefaultQuery("limit", "20")
+	limit, _ := strconv.Atoi(limit_str)
+	notes, count, err := noteService.SenderGet(ctx, offset, limit, user_id.(string))
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2002,
+			"msg":  err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code":   2000,
+		"msg":    "成功",
+		"count":  count,
+		"notes":  notes,
+		"offset": offset,
+		"limit":  limit,
+	})
 }
