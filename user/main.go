04cedb3b74ccf68e83801d7ae0247963f9aa8364
Cunoe <cunkycheng@gmail.com>
2022-05-26 10:34:53 +0800

    fix bug

--- user/main.go
+++ user/main.go
@@ -11,10 +11,12 @@ import (
 func main() {
 	// start server
 	config := loadConfig("./config.yaml")
+	log.Printf("App: %v", config.App.Name)
 	lis, err := net.Listen("tcp", config.GrpcSet.EndPoint)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	log.Printf("listen in %v", config.GrpcSet.EndPoint)
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
 	// pb.RegisterGreeterServer(grpcServer, newServer())
