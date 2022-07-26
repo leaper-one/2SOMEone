da764cd7eee4120e67b0bbfe08a4e2d661060336
Cunoe <cunkycheng@gmail.com>
2022-07-26 18:30:12 +0800

    database change

--- rpc/message-rpc/internal/logic/checkmessagecodelogic.go
+++ rpc/message-rpc/internal/logic/checkmessagecodelogic.go
@@ -2,11 +2,10 @@ package logic
 
 import (
 	"context"
+	"time"
 
 	"github.com/leaper-one/2SOMEone/rpc/message-rpc/internal/svc"
 	"github.com/leaper-one/2SOMEone/rpc/message-rpc/types/message"
-	msg_service "github.com/leaper-one/2SOMEone/service/message"
-
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -26,7 +25,9 @@ func NewCheckMessageCodeLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 
 //  校验验证码
 func (l *CheckMessageCodeLogic) CheckMessageCode(in *message.CheckMessageCodeRequest) (*message.CheckMessageCodeResponse, error) {
-	is_match, err := msg_service.CheckPhoneCode(l.ctx, in.Phone, in.Code, uint(in.MsgId))
+
+	code, phone, timestamp, err := l.svcCtx.Model.FindCode(context.Background(), int64(in.MsgId))
+
 	if err != nil {
 		return &message.CheckMessageCodeResponse{
 			Code:    500,
@@ -34,9 +35,35 @@ func (l *CheckMessageCodeLogic) CheckMessageCode(in *message.CheckMessageCodeReq
 			IsMatch: false,
 		}, err
 	}
+
+	// 5分钟内有效
+	if time.Now().Unix()-timestamp > 300 {
+		return &message.CheckMessageCodeResponse{
+			Code:    500,
+			Msg:     "验证码已过期",
+			IsMatch: false,
+		}, nil
+	}
+
+	if phone != in.Phone {
+		return &message.CheckMessageCodeResponse{
+			Code:    200,
+			Msg:     "手机号不匹配",
+			IsMatch: false,
+		}, nil
+	}
+
+	if code != in.Code {
+		return &message.CheckMessageCodeResponse{
+			Code:    200,
+			Msg:     "验证码不匹配",
+			IsMatch: false,
+		}, nil
+	}
+
 	return &message.CheckMessageCodeResponse{
 		Code:    200,
 		Msg:     "success",
-		IsMatch: is_match,
+		IsMatch: true,
 	}, nil
 }
--- rpc/message-rpc/internal/logic/sentmessagecodelogic.go
+++ rpc/message-rpc/internal/logic/sentmessagecodelogic.go
@@ -2,7 +2,6 @@ package logic
 
 import (
 	"context"
-
 	"github.com/leaper-one/2SOMEone/rpc/message-rpc/internal/svc"
 	"github.com/leaper-one/2SOMEone/rpc/message-rpc/types/message"
 	msg_service "github.com/leaper-one/2SOMEone/service/message"
@@ -26,7 +25,8 @@ func NewSentMessageCodeLogic(ctx context.Context, svcCtx *svc.ServiceContext) *S
 
 //  向手机号发送验证码
 func (l *SentMessageCodeLogic) SentMessageCode(in *message.SentMessageCodeRequest) (*message.SentMessageCodeResponse, error) {
-	_, msg_id, err := msg_service.SendPhoneCode(l.ctx, in.Phone)
+	code, err := msg_service.SendPhoneCode(l.ctx, in.Phone)
+
 	if err != nil {
 		return &message.SentMessageCodeResponse{
 			Code:  500,
@@ -35,9 +35,20 @@ func (l *SentMessageCodeLogic) SentMessageCode(in *message.SentMessageCodeReques
 		}, err
 	}
 
+	ret, err := l.svcCtx.Model.InsertCode(context.Background(), in.Phone, code)
+
+	if err != nil {
+		return &message.SentMessageCodeResponse{
+			Code:  500,
+			Msg:   "存储验证码失败",
+			MsgId: 0,
+		}, err
+	}
+	msgId, _ := ret.LastInsertId()
+
 	return &message.SentMessageCodeResponse{
 		Code:  200,
 		Msg:   "success",
-		MsgId: msg_id,
+		MsgId: uint64(msgId),
 	}, nil
 }
