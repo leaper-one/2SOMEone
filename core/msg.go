da1b9f9a6dca7aa97b1f3c51883e58fefbe4bec2
Cunoe <cunkycheng@gmail.com>
2022-08-02 16:59:33 +0800

    fix bug

--- core/msg.go
+++ core/msg.go
@@ -2,16 +2,17 @@ package core
 
 import (
 	"context"
-	"gorm.io/gorm"
 )
 
 type (
 	Message struct {
-		gorm.Model
-		Phone   string `gorm:"size:14;index" json:"phone,omitempty"`
-		Type    uint8  `gorm:"type:tinyint(1);default:0" json:"type,omitempty"`
-		Content string `gorm:"size:512"`
-		Code    string `gorm:"size:6"`
+		//gorm.Model
+		ID        uint   `gorm:"primarykey"`
+		Timestamp int64  `gorm:"default:0"`
+		Phone     string `gorm:"size:14;index" json:"phone,omitempty"`
+		Type      uint8  `gorm:"type:tinyint(1);default:0" json:"type,omitempty"`
+		Content   string `gorm:"size:512"`
+		Code      string `gorm:"size:6"`
 	}
 
 	MessageStore interface {
--- rpc/message-rpc/internal/logic/checkmessagecodelogic.go
+++ rpc/message-rpc/internal/logic/checkmessagecodelogic.go
@@ -36,19 +36,19 @@ func (l *CheckMessageCodeLogic) CheckMessageCode(in *message.CheckMessageCodeReq
 		}, err
 	}
 
+	if phone != in.Phone {
+		return &message.CheckMessageCodeResponse{
+			Code:    200,
+			Msg:     "手机号不匹配",
+			IsMatch: false,
+		}, nil
+	}
+
 	// 5分钟内有效
 	if time.Now().Unix()-timestamp > 300 {
 		return &message.CheckMessageCodeResponse{
 			Code:    500,
 			Msg:     "验证码已过期",
-			IsMatch: false,
-		}, nil
-	}
-
-	if phone != in.Phone {
-		return &message.CheckMessageCodeResponse{
-			Code:    200,
-			Msg:     "手机号不匹配",
 			IsMatch: false,
 		}, nil
 	}
